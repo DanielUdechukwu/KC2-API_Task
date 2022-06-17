3a830af01da8e95e65af6edf4f6a09bf15b66216
Daniel <danieludechukwu117@gmail.com>
2022-06-17 14:23:11 +0100

    Final Changes

--- js/script.js
+++ js/script.js
@@ -12,7 +12,7 @@ function getPosts() {
   fetch('https://jsonplaceholder.typicode.com/posts')
   .then((response) => response.json())
   .then((data = data) => {console.log(data)
-    postBox = data.splice(0, 10);
+    postBox = data.splice(0,20);
     render(postBox);
 
   })
@@ -33,9 +33,9 @@ function create(event) {
       body: body.value,
       userId: 2,
     }),
-    headers: {
-      'Content-type': 'application/json; charset=UTF-8',
-    }
+    // headers: {
+    //   'Content-type': 'application/json; charset=UTF-8',
+    // }
   })
     .then((response) => response.json())
     .then((data) => { console.log(data)
@@ -58,17 +58,14 @@ function updatePost(id){
     body: body.value,
     userId: 2,
   }),
-  headers: {
-    'Content-type': 'application/json; charset=UTF-8',
-  },
+  // headers: {
+  //   'Content-type': 'application/json; charset=UTF-8',
+  // },
 })
   .then((response) => response.json())
   .then((data) => {
-  
-    //console.log(data)
     let postTitle = document.querySelectorAll("#post-title");
     let postBody = document.querySelectorAll("#post-body");
-    //console.log(postTitle)
 
     postTitle.forEach((postTitle, index) => {
       if (index +1 === id){
@@ -100,7 +97,6 @@ function deletePost(id){
   })
   .then((response) => response.json())
   .then((data) => {
-    //console.log(data)
     postBox = postBox.filter(post => post.id !== id)
     render(postBox);
   })
@@ -113,11 +109,8 @@ function viewPost(id){
   fetch(`https://jsonplaceholder.typicode.com/posts/${id}`)
     .then((response) => response.json())
     .then((data) => {
-      //console.log(data)
       localStorage.setItem("viewedPost", JSON.stringify(data))
       window.location.href = `./update/update.html?id=${id}`
-      // postBox = postBox.filter(post => post.id !== id)
-      //render(postBox);
     })
 }
 
@@ -136,13 +129,13 @@ function render(myArr){
 
           <div class="d-flex justify-content-end" style="gap: 1rem;">
             <button class="" id="view" onclick="viewPost(${post.id})">
-              <img src="./images/eye.svg" alt="">
+              <i class="fa-regular fa-eye" style="font-size: 2rem;"></i>
             </button>
             <button class="" id="update" onclick="updatePost(${post.id})">
-              <img src="./images/refresh.svg" alt="">
+            <i class="fa-solid fa-arrows-rotate" style="font-size: 2rem;"></i>
             </button>
-            <button class="" id="delete" onclick="deletePost(${post.id})">
-              <img src="./images/delete.svg" alt="">
+            <button class="" id="delete" onclick="deletePost(${post.id})"> 
+            <i class="fa-regular fa-trash-can" style="font-size: 2rem;"></i>
             </button>
           </div>
           <p class="fw-bold">${post.id}</p>
